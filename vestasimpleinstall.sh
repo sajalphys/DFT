3aea942dd8f5ba8017340e653f57002d41765680
Sajal Biswas <sajalphys@gmail.com>
2024-11-24 09:40:07 +0530

    Add files via upload

---
+++ vestasimpleinstall.sh
@@ -0,0 +1,8 @@
+###################This Code Helps You in installing Vesta in your Linux System #############
+########### Run chmod +x vestasimpleinstall.sh in the directory containing the script #######
+########### Run sh vestasimpleinstall.sh ####################################################
+########### Script Written by - Sajal Biswas, Assistant Prof. of Physics, KWC, Nadia, WB ####
+########### For any query Email :- sajalphys@gmail.com ######################################
+wget https://sf.net/projects/materiappslive/files/Debian/sources/setup.sh
+sudo sh setup.sh
+sudo apt -y install vesta
